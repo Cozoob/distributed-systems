62dd16479963713d48ec7931e23e8d059c47664a
Marcin Kozub <kozubmarcin10@gmail.com>
2023-03-19 18:34:12 +0100

    Display data from one service already

--- FastAPI/routers/meteo.py
+++ FastAPI/routers/meteo.py
@@ -1,42 +1,91 @@
 import requests
 from fastapi import APIRouter
 from config.settings import get_settings
+from json import JSONDecodeError
+from starlette.templating import Jinja2Templates
+from schemas.responses import ForecastExternalResponse
 
 meteo_source_config = get_settings().meteo_source_config
 URL = meteo_source_config.url
 API_KEY = meteo_source_config.api_key
 
 router = APIRouter()
+templates = Jinja2Templates(directory="./websites")
+ERROR_PAGE = "error.html"
 
 
-# async def request(session: aiohttp.ClientSession) -> str:
-#     async with session.get(URL) as response:
-#         return await response.text()
-#
-#
-# async def task():
-#     async with aiohttp.ClientSession() as session:
-#         tasks = [request(session) for _ in range(100)]
-#         return await asyncio.gather(*tasks)
-#
-#
-# @router.get("/uuids")
-# async def get_uuids():
-#     return {"uuids": await task()}
+class MeteoForecast:
 
-# @router.get("/weather")
-# async def submit_form():
-#
-class MeteoWeather:
+    def get_forecast(self, city_name: str, days: int) -> dict:
+        # find the city id
+        response = requests.get(
+            URL + "/find_places",
+            params={
+                'key': API_KEY,
+                'language': 'en',
+                'text': city_name
+            }
+        )
 
-    def get_forecast(self, city_name: str, days: int):
-        ...
+        forecast_val = {"is_error": False, "details": {}}
 
+        if response.status_code != 200:
+            forecast_val["is_error"] = True
+            forecast_val["details"]["message"] = "Error while connecting to external API. Contact your administrator."
+            print("Status code when finding city by name: ", response.status_code)
+            try:
+                print(response.json())
+            except JSONDecodeError:
+                print(response.content)
 
-@router.get("/meteo/{city}")
-async def get_london(city: str):
-    params = {
-        'key': API_KEY,
-        'place_id': 'london'
-    }
-    return requests.get(URL, params).json()
+            return forecast_val
+
+        found_cities = response.json()
+
+        if not found_cities:
+            forecast_val["is_error"] = True
+            forecast_val["details"]["message"] = "Error while finding your city. It may not exist."
+            return forecast_val
+
+        city_id = found_cities[0]["place_id"]
+
+        # check the city's forecast
+        response = requests.get(
+            URL + "/point",
+            params={
+                'key': API_KEY,
+                'language': 'en',
+                'place_id': city_id,
+                "sections": 'daily',
+                "units": "metric"
+            }
+        )
+
+        if response.status_code != 200:
+            forecast_val["is_error"] = True
+            forecast_val["details"]["message"] = "Error while connecting to external API. Contact your administrator."
+            print("Status code when finding city by name: ", response.status_code)
+            try:
+                print(response.json())
+            except JSONDecodeError:
+                print(response.content)
+
+            return forecast_val
+
+        daily_forecast = response.json()["daily"]["data"][:days]
+        forecast_val["details"]["value"] = []
+        results = forecast_val["details"]["value"]
+
+        for val in daily_forecast:
+            results.append(
+                ForecastExternalResponse(
+                    val["day"],
+                    val["all_day"]["temperature_min"],
+                    val["all_day"]["temperature_max"],
+                    val["all_day"]["temperature"],
+                    val["summary"],
+                    val["all_day"]["wind"]["speed"]
+                )
+            )
+
+        return forecast_val
--- FastAPI/routers/server.py
+++ FastAPI/routers/server.py
@@ -2,10 +2,13 @@ from fastapi import APIRouter
 from starlette.requests import Request
 from starlette.responses import HTMLResponse, Response
 from starlette.templating import Jinja2Templates
-from starlette.datastructures import URL
+from routers.meteo import MeteoForecast
+from schemas.responses import WeatherResponse
 
 router = APIRouter()
 templates = Jinja2Templates(directory="./websites")
+ERROR_PAGE = "error.html"
+EXTERNAL_SERVICES = [MeteoForecast()]
 
 
 @router.get("/", response_class=HTMLResponse)
@@ -13,20 +16,82 @@ async def root(request: Request) -> Response:
     return templates.TemplateResponse("inputForm.html", {"request": request})
 
 
-@router.get("/weather")
+@router.get("/weather", response_class=HTMLResponse)
 async def submit_form(request: Request, city: str, days: int):
-
+    global summary
     if not city.isalpha():
         # check if contains only letters
-        return templates.TemplateResponse("error.html", {"request": request})
+        return templates.TemplateResponse(ERROR_PAGE, {
+            "request": request,
+            "message": "The city's name is incorrect!"
+        })
 
     if not (1 <= days <= 5):
         # incorrect days param
-        return templates.TemplateResponse("error.html", {"request": request})
+        return templates.TemplateResponse(ERROR_PAGE, {
+            "request": request,
+            "message": "The number of days is incorrect!"
+        })
 
-    return templates.TemplateResponse("city.html", {"request": request, "val": "suprise"})
+    # get forecast and return information
+    fetched_data = {}
+    for service in EXTERNAL_SERVICES:
+        forecast_val = service.get_forecast(city_name=city, days=days)
 
-    # if not city:
-    #     return templates.TemplateResponse("inputForm.html", {"message": "error"})
-    #
-    # return {"chosen city": city}
+        if forecast_val["is_error"]:
+            # show error page with error message
+            return templates.TemplateResponse(ERROR_PAGE, {
+                "request": request,
+                "message": forecast_val["details"]["message"]
+            })
+
+        results = forecast_val["details"]["value"]
+
+        # TODO sprawdz czy daty z drugiego serwisu sa takie same!
+        for res in results:
+            info = fetched_data.get(res.date, [])
+            info.append(res)
+            fetched_data[res.date] = info
+
+    results = {}
+    for date, info in fetched_data.items():
+        amount_of_data = len(info)
+
+        # averages
+        avg_temp = sum(map(lambda response: response.temp, info)) / amount_of_data
+        avg_min_temp = sum(map(lambda response: response.temp_min, info)) / amount_of_data
+        avg_max_temp = sum(map(lambda response: response.temp_max, info)) / amount_of_data
+        avg_wind_speed = sum(map(lambda response: response.wind_speed, info)) / amount_of_data
+
+        # variances
+        var_temp = sum(map(lambda response: (response.temp - avg_temp) ** 2, info)) / amount_of_data
+        var_min_temp = sum(map(lambda response: (response.temp_min - avg_min_temp) ** 2, info)) / amount_of_data
+        var_max_temp = sum(map(lambda response: (response.temp_max - avg_max_temp) ** 2, info)) / amount_of_data
+        var_wind_speed = sum(map(lambda response: (response.wind_speed - avg_wind_speed) ** 2, info)) / amount_of_data
+
+        for response in info:
+            if response.summary:
+                summary = response.summary
+                break
+            else:
+                summary = "Summary not found"
+
+        res = results.get(date, [])
+        res.append(
+            WeatherResponse(
+                city=city,
+                date=date,
+                avg_temp_min=avg_min_temp,
+                var_temp_min=var_min_temp,
+                avg_temp_max=avg_max_temp,
+                var_temp_max=var_max_temp,
+                avg_temp=avg_temp,
+                var_temp=var_temp,
+                summary=summary,
+                avg_wind_speed=avg_wind_speed,
+                var_wind_speed=var_wind_speed
+            )
+        )
+        results[date] = res
+    print(results)
+    return templates.TemplateResponse("city.html", {"request": request, "data": results, "city": city})
